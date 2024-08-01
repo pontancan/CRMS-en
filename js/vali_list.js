9d8601a14647879c37f6427d7585070db8cf7ef4
Kotaro <you@example.com>
2024-08-01 15:30:32 +0900

    検索バリデーションmade

---
+++ js/vali_list.js
@@ -0,0 +1,146 @@
+let input_name = document.getElementById('name');
+let name_message = document.getElementById('name_message');
+let input_kana = document.getElementById('kana');
+let kana_message = document.getElementById('kana_message');
+let input_gender = document.getElementById('gender');
+let gender_message = document.getElementById('gender_message');
+let input_dob_start = document.getElementById('dob_start');
+let dob_message = document.getElementById('dob_message');
+let input_dob_end = document.getElementById('dob_end');
+// let dob_end_message = document.getElementById('dob_end_message');
+let input_company = document.getElementById('company');
+let company_message = document.getElementById('company_message');
+let input_submit = document.getElementById('searchForm');
+let submit_message = document.getElementById('submit_message');
+
+// 各フィールドのバリデーション結果を保持するフラグ
+let isNameValid = false;
+let isKanaValid = false;
+let isGenderValid = false;
+let isDobValid = false;
+let isCompanyValid = false;
+
+function validateName() {
+    if (input_name.value.trim() === '') {
+        name_message.innerText = '入力は必須です';
+        isNameValid = false;
+    } else if (input_name.value.trim().length < 3) {
+        name_message.innerText = '3文字以上必要です';
+        isNameValid = false;
+    } else {
+        name_message.innerText = '';
+        isNameValid = true;
+    }
+}
+
+function validateKana() {
+    if (input_kana.value.trim() === '') {
+        kana_message.innerText = '入力は必須です';
+        isKanaValid = false;
+    } else if (input_kana.value.trim().length < 3) {
+        kana_message.innerText = '3文字以上必要です';
+        isKanaValid = false;
+    } else {
+        kana_message.innerText = '';
+        isKanaValid = true;
+    }
+}
+
+
+
+function validateGender() {
+    if (input_gender.value.trim() === '') {
+        gender_message.innerText = '選択は必須です';
+        isGenderValid = false;
+    } else {
+        gender_message.innerText = '';
+        isGenderValid = true;
+    }
+}
+
+function validateDob() {
+    if (input_dob_start.value.trim() === '' || input_dob_end.value.trim() === '') {
+        dob_message.innerText = '入力は必須です';
+        isDobValid = false;
+    }else {
+        dob_message.innerText = '';
+        isDobValid = true;
+    }
+}
+
+function validateCompany() {
+    if (input_company.value.trim() === '') {
+        company_message.innerText = '選択は必須です';
+        isCompanyValid = false;
+    } else {
+        company_message.innerText = '';
+        isCompanyValid = true;
+    }
+}
+
+let messages = [];
+
+function updateSubmitMessage() {
+    messages = [];
+    if (!isNameValid) messages.push("顧客名が無効です");
+    if (!isKanaValid) messages.push("顧客名カナが無効です");
+    if (!isGenderValid) messages.push("性別が無効です");
+    if (!isDobValid) messages.push("生年月日が無効です");
+    if (!isCompanyValid) messages.push("所属会社が無効です");
+    submit_message.innerText = messages.join("\n");
+}
+//TODO 無効ならフィールド背景赤に
+
+function validateAll() {
+    validateName();
+    validateKana();
+    validateGender();
+    validateDob();
+    validateCompany();
+    updateSubmitMessage();
+}
+
+input_name.addEventListener('focusout', () => {
+    validateName();
+    updateSubmitMessage();
+});
+
+input_kana.addEventListener('focusout', () => {
+    validateKana();
+    updateSubmitMessage();
+});
+
+
+
+input_gender.addEventListener('focusout', () => {
+    validateGender();
+    updateSubmitMessage();
+});
+
+input_dob_start.addEventListener('focusout', () => {
+    validateDob();
+    updateSubmitMessage();
+});
+input_dob_end.addEventListener('focusout', () => {
+    validateDob();
+    updateSubmitMessage();
+});
+
+
+
+input_company.addEventListener('focusout', () => {
+    validateCompany();
+    updateSubmitMessage();
+});
+
+input_submit.addEventListener("submit", (e) => {
+    validateAll();
+
+    if (messages.length > 0) {
+
+        e.preventDefault();
+    }
+});
+
+
+
