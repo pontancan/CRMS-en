b8d8fb58009118e4ced35fd98e7f8fc89a482c8a
Kotaro <you@example.com>
2024-08-01 13:01:04 +0900

    プロセスバー削除前

--- js/vali.js
+++ js/vali.js
@@ -1,151 +1,182 @@
-console.log('hello world');
-
-
 let input_name = document.getElementById('name');
 let name_message = document.getElementById('name_message');
-
-let flag = false;
-
-
-
-input_name.addEventListener('focusout', () => {
-    if (input_name.value.trim() === '') {
-        name_message.innerText = '入力は必須です';
-        flag = false;
-    } else if (input_name.value.trim().length < 3) {
-        name_message.innerText = '3文字以上必要です';
-        flag = false;
-    } else {
-        name_message.innerText = '';
-        flag = true;
-    }
-});
-
 let input_kana = document.getElementById('kana');
 let kana_message = document.getElementById('kana_message');
-
-input_kana.addEventListener('focusout', () => {
-    if (input_kana.value.trim() === '') {
-        kana_message.innerText = '入力は必須です';
-        flag = false;
-    } else if (input_kana.value.trim().length < 3) {
-        kana_message.innerText = '3文字以上必要です';
-        flag = false;
-    } else {
-        kana_message.innerText = '';
-        flag = true;
-    }
-});
-
 let input_email = document.getElementById('email');
 let email_message = document.getElementById('email_message');
-
-function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
-
-input_email.addEventListener('focusout', () => {
-    if (input_email.value.trim() === '') {
-        email_message.innerText = '入力は必須です';
-        flag = false;
-    } else if (!validateEmail(input_email.value.trim())) {
-        email_message.innerText = '有効なメールアドレスではありません';
-        flag = false;
-    } else {
-        email_message.innerText = '';
-        flag = true;
-    }
-});
-
 let input_phone = document.getElementById('phone');
 let phone_message = document.getElementById('phone_message');
-
-function validatePhone(phone) {
-    const re = /^\d{9,10}$/;
-    return re.test(phone);
-}
-
-input_phone.addEventListener('focusout', () => {
-    if (input_phone.value.trim() === '') {
-        phone_message.innerText = '入力は必須です';
-        flag = false;
-    } else if (!validatePhone(input_phone.value.trim())) {
-        phone_message.innerText = '有効な電話番号ではありません';
-        flag = false;
-    } else {
-        phone_message.innerText = '';
-        flag = true;
-    }
-});
-
-
 let input_gender = document.getElementById('gender');
 let gender_message = document.getElementById('gender_message');
-
-
-
-input_gender.addEventListener('focusout', () => {
-    if (input_gender.value.trim() === '') {
-        gender_message.innerText = '選択は必須です';
-        flag = false;
-    } else {
-        gender_message.innerText = '';
-        flag = true;
-    }
-});
-
 let input_dob = document.getElementById('dob');
 let dob_message = document.getElementById('dob_message');
-
-
-
-input_dob.addEventListener('focusout', () => {
-    if (input_dob.value.trim() === '') {
-        dob_message.innerText = '入力は必須です';
-        flag = false;
-    } else {
-        dob_message.innerText = '';
-        flag = true;
-    }
-});
-
-
 let input_company = document.getElementById('company');
 let company_message = document.getElementById('company_message');
-
-
-
-input_company.addEventListener('focusout', () => {
-    if (input_company.value.trim() === '') {
-        company_message.innerText = '選択は必須です';
-        flag = false;
-    } else {
-        company_message.innerText = '';
-        flag = true;
-    }
-});
-
 let input_submit = document.getElementById('registerForm');
 let submit_message = document.getElementById('submit_message');
 
-input_submit.addEventListener("submit", (e) => {
-    if (!flag) {
-        e.preventDefault();
-        submit_message.innerText = '入力漏れがあります';
-    } 
-    
+// 各フィールドのバリデーション結果を保持するフラグ
+let isNameValid = false;
+let isKanaValid = false;
+let isEmailValid = false;
+let isPhoneValid = false;
+let isGenderValid = false;
+let isDobValid = false;
+let isCompanyValid = false;
+
+function validateName() {
+    if (input_name.value.trim() === '') {
+        name_message.innerText = '入力は必須です';
+        isNameValid = false;
+    } else if (input_name.value.trim().length < 3) {
+        name_message.innerText = '3文字以上必要です';
+        isNameValid = false;
+    } else {
+        name_message.innerText = '';
+        isNameValid = true;
+    }
+}
+
+function validateKana() {
+    if (input_kana.value.trim() === '') {
+        kana_message.innerText = '入力は必須です';
+        isKanaValid = false;
+    } else if (input_kana.value.trim().length < 3) {
+        kana_message.innerText = '3文字以上必要です';
+        isKanaValid = false;
+    } else {
+        kana_message.innerText = '';
+        isKanaValid = true;
+    }
+}
+
+function validateEmail() {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (input_email.value.trim() === '') {
+        email_message.innerText = '入力は必須です';
+        isEmailValid = false;
+    } else if (!re.test(input_email.value.trim())) {
+        email_message.innerText = '有効なメールアドレスではありません';
+        isEmailValid = false;
+    } else {
+        email_message.innerText = '';
+        isEmailValid = true;
+    }
+}
+
+function validatePhone() {
+    const re = /^\d{9,10}$/;
+    if (input_phone.value.trim() === '') {
+        phone_message.innerText = '入力は必須です';
+        isPhoneValid = false;
+    } else if (!re.test(input_phone.value.trim())) {
+        phone_message.innerText = '有効な電話番号ではありません';
+        isPhoneValid = false;
+    } else {
+        phone_message.innerText = '';
+        isPhoneValid = true;
+    }
+}
+
+function validateGender() {
+    if (input_gender.value.trim() === '') {
+        gender_message.innerText = '選択は必須です';
+        isGenderValid = false;
+    } else {
+        gender_message.innerText = '';
+        isGenderValid = true;
+    }
+}
+
+function validateDob() {
+    if (input_dob.value.trim() === '') {
+        dob_message.innerText = '入力は必須です';
+        isDobValid = false;
+    } else {
+        dob_message.innerText = '';
+        isDobValid = true;
+    }
+}
+
+function validateCompany() {
+    if (input_company.value.trim() === '') {
+        company_message.innerText = '選択は必須です';
+        isCompanyValid = false;
+    } else {
+        company_message.innerText = '';
+        isCompanyValid = true;
+    }
+}
+
+let messages = [];
+
+function updateSubmitMessage() {
+    messages = [];
+    if (!isNameValid) messages.push("顧客名が無効です");
+    if (!isKanaValid) messages.push("顧客名カナが無効です");
+    if (!isEmailValid) messages.push("メールアドレスが無効です");
+    if (!isPhoneValid) messages.push("電話番号が無効です");
+    if (!isGenderValid) messages.push("性別が無効です");
+    if (!isDobValid) messages.push("生年月日が無効です");
+    if (!isCompanyValid) messages.push("所属会社が無効です");
+    submit_message.innerText = messages.join("\n");
+}
+//TODO 無効ならフィールド背景赤に
+
+function validateAll() {
+    validateName();
+    validateKana();
+    validateEmail();
+    validatePhone();
+    validateGender();
+    validateDob();
+    validateCompany();
+    updateSubmitMessage();
+}
+
+input_name.addEventListener('focusout', () => {
+    validateName();
+    updateSubmitMessage();
 });
 
+input_kana.addEventListener('focusout', () => {
+    validateKana();
+    updateSubmitMessage();
+});
 
+input_email.addEventListener('focusout', () => {
+    validateEmail();
+    updateSubmitMessage();
+});
 
+input_phone.addEventListener('focusout', () => {
+    validatePhone();
+    updateSubmitMessage();
+});
 
+input_gender.addEventListener('focusout', () => {
+    validateGender();
+    updateSubmitMessage();
+});
 
+input_dob.addEventListener('focusout', () => {
+    validateDob();
+    updateSubmitMessage();
+});
 
+input_company.addEventListener('focusout', () => {
+    validateCompany();
+    updateSubmitMessage();
+});
 
+input_submit.addEventListener("submit", (e) => {
+    validateAll();
 
+    if (messages.length > 0) {
 
-
+        e.preventDefault();
+    }
+});
 
 
 
