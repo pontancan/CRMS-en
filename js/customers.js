f5530ed03d5d29cc44aa7aae137eead3bdce1ca5
Kotaro <you@example.com>
2024-08-02 13:52:49 +0900

    tr追加

---
+++ js/customers.js
@@ -0,0 +1,32 @@
+export const customers = [
+    {
+        'id': '0001',
+        'name': '山田太郎',
+        'kana': 'ヤマダタロウ',
+        'email': 'tyamada@test.com',
+        'phone': '080-4444-0001',
+        'company': '山田商店',
+        'created_at': '2024/07/01',
+        'updated_at': '2024/07/01'
+    },
+    {
+        'id': '0002',
+        'name': '山田花子',
+        'kana': 'ヤマダハナコ',
+        'email': 'hyamada@test.com',
+        'phone': '080-4444-0002',
+        'company': '山田商店',
+        'created_at': '2024/07/02',
+        'updated_at': '2024/07/03'
+    },
+    {
+        'id': '0003',
+        'name': '吉田茂',
+        'kana': 'ヨシダシゲル',
+        'email': 'syoshida@test.com',
+        'phone': '080-4444-0003',
+        'company': '鈴木生花店',
+        'created_at': '2024/07/02',
+        'updated_at': '2024/07/02'
+    },
+];
